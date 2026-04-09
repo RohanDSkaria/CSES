700e8e48d86528a06cf49af4725dbb9020bd981a
RohanDSkaria <rohandanny484@gmail.com>
2026-04-09 18:17:23 +0530

    reset

--- 1_Introductory_Problems/Two_Knights.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int n; cin>>n;
-    for(long long i=1; i<=n; i++){
-        cout<<i*i*(i*i-1)/2 - 8*((i-2)*(i-1)/2)<<endl;
-    }
-    return 0;
-}
